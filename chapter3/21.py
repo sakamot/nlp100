e8cc9392c256cebeb186afd8405115c480c0567d
sakamot <matuhana.8@gmail.com>
2018-01-26 23:37:23 +0900

    add chapter3/21

---
+++ chapter3/21.py
@@ -0,0 +1,11 @@
+import json
+import re
+from q20 import load_file
+
+split_text = load_file().splitlines()
+pattern = r"\[\[Category"
+repattern = re.compile(pattern)
+
+for t in split_text:
+    if repattern.match(t):
+        print(t)
