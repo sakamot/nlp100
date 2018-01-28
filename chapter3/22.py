b03deae5dff6c9621ddb06ca8b31abd3524c2726
sakamot <matuhana.8@gmail.com>
2018-01-28 16:42:51 +0900

    refactor chapter3/22

--- chapter3/22.py
+++ chapter3/22.py
@@ -2,10 +2,10 @@ import re
 from q20 import load_file
 
 split_text = load_file().splitlines()
-pattern = r"\[\[Category:(?P<name>.*)\]\]"
+pattern = r"\[\[Category:(.*?)\]\]"
 repattern = re.compile(pattern)
 
 for t in split_text:
     match = repattern.match(t)
     if match:
-        print(match.group('name'))
+        print(match[1])
