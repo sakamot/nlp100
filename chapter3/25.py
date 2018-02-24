782bd92fdd5268c4c8118c65c17e2ef1d3834a9e
sakamot <matuhana.8@gmail.com>
2018-02-24 21:07:05 +0900

    add chapter3/25

---
+++ chapter3/25.py
@@ -0,0 +1,13 @@
+import re
+from q20 import load_file
+
+pattern = r"^\{\{基礎情報.*?$(.*?)^\}\}$"
+repattern = re.compile(pattern, re.MULTILINE + re.VERBOSE + re.DOTALL)
+
+match = repattern.findall(load_file())
+pat = r"^\|(.+?) = (.+?)\n"
+repat = re.compile(pat, re.MULTILINE + re.VERBOSE + re.DOTALL)
+match1 = repat.findall(match[0])
+
+dic = { m[0]: m[1] for m in match1 }
+print(dic)
